736a7c7396256166cb58ead7e41d821b83d24fd9
Edmundo Jose Batista <eddmundosjb@gmail.com>
2023-08-15 16:10:33 -0300

    feat: encerrando-por-agora

--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import styles from './User.module.css';
+import { useState } from 'react'
+import styles from './User.module.css'
 
 export default function User({ user }) {
-  const [likeAmount, setLikeAmount] = useState(0); 
-  
+  const [likeAmount, setLikeAmount] = useState(0);
+
   const updateLikeAmount = (likeOption) => {
     if (likeOption === 'down' && likeAmount - 1 >= 0) {
       setLikeAmount((likeAmount) => likeAmount - 1)
@@ -12,18 +12,17 @@ export default function User({ user }) {
       setLikeAmount((likeAmount) => likeAmount + 1)
     }
   }
-  console.log(likeAmount);
-
 
   return (
     <div className={styles.card}>
       <div className={styles.imageContainer}>
         <img className={styles.cardUserImage} src={user.image} alt={`imagem do usuário ${user.userName}`} />
       </div>
-        <h3>Oi eu sou {user.userName} e tenho {likeAmount} like{likeAmount > 1 ? 's' : ''} </h3>
+      <h3>Oi eu sou {user.userName} e tenho {likeAmount} like{likeAmount > 1 ? 's' : ''}</h3>
+
       <div className={styles.buttonContainer}>
         <button className={`${styles.button} ${styles.likeButton}`} onClick={() => updateLikeAmount('up')}>Like 👍</button>
-        <button className={`${styles.button} ${styles.dislikeButton}`} onClick={() => updateLikeAmount('down')}>DisLike 👎</button>
+        <button className={`${styles.button} ${styles.dislikeButton}`} onClick={() => updateLikeAmount('down')}>Dislike 👎</button>
       </div>
     </div>
   )
